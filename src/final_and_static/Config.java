30ba0b3f459880e13ca3d0397295ca46d4584812
Đức Anh Tài <39086667+datai999@users.noreply.github.com>
2020-07-16 15:48:22 +0700

    Key word final and static

---
+++ src/final_and_static/Config.java
@@ -0,0 +1,13 @@
+package final_and_static;
+
+public final class Config {
+
+    static int JAVA_VERSION = 9;
+    static String MAVEN_VERSION = "3.6.2";
+    static String IDE = "Intellij";
+
+    static float PI = 3.14f;
+    static String LINK_SOURCE = "https://github.com/datai999/citynow-intern-study";
+    static String LINK_PROCESS = "https://github.com/datai999/citynow-intern-study/projects/2";
+
+}
